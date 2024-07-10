0714f502e054291ffd6ca7c07021195e2979f551
Steven McIntyre <steven.mc2225@gmail.com>
2024-07-10 00:33:24 -0700

    rewrote lots to simplify and organize better

--- scripts.js
+++ scripts.js
@@ -3,98 +3,76 @@
 //
 const GameBoard = (function(){
   // Variables
-  const gameBoard = [];
+  const board = [];
   const rows = 3;
   const columns = 3;
-  const [player1, player2] = playerSetup(); // assign the returned players to usable variables
+  const [player1, player2] = players(); // assign the returned players to usable variables
   
-  function playerSetup() { // set player info and return array of players     **inputs need to be sanitised**
-    let playerOneDefaultOrder = 0;
-    let playerTwoDefaultOrder = 1;
-    
-    const player1 = {
-      name: "Player 1", // temp name
-      mark: "X",   // temp mark
-      identifier: 1,
-      playOrder: playerOneDefaultOrder,
-    }
-    
-    const player2 = {
-      name: "Player 2", // temp name
-      mark: "O",     // temp mark
-      identifier: 2,
-      playOrder: playerTwoDefaultOrder,
-    }
-    return [player1, player2];
-  }
-  
+
   for (let i = 0; i < rows; i++) { // Setting up game board START
-    gameBoard[i] = [];
+    board[i] = [];
     for (let j = 0; j < columns; j++) {
-      gameBoard[i][j] = 0;
+      board[i][j] = 0;
     }
   }
 
-
-  function placeMark () { // place a mark on the board. REQUIRES => (PLAYER)(ROW)(COLUMN)
-    const currentPlayer = prompt("Who's turn is it?");
-    const row = prompt("row?");
-    const column = prompt("column?");
-    let player;
-    if (currentPlayer === "2") {
-      player = player2;
-    } else if (currentPlayer === "1"){
-      player = player1;
-    } else {
-      console.error("error");
-      
-    }
-    gameBoard[row][column] = player.identifier; // player not yet defined
+  function placeMark(player, row, column) { 
+    board[row][column] = player.identifier; 
   }
 
-  function logBoard() {
-    console.log(gameBoard);
+  function logBoard() { // log the game board readably
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
   }
 
-  return { placeMark, logBoard, showPlayers };
+  return { placeMark, logBoard};
 
 })();
 
-
-
-
-
-
-
-
-
-
-
-GameInit.showPlayers();
-// GameInit.placeMark();
-// GameInit.logBoard();
-
-
-
-// *** BluePrint ***
-
-// * Objects *
-//  - gameBoard
-//  - game
-//  - player
 //
-
-
-// * LOGIC *
-
-// PLAY ROUND
+// PLAYERS
 //
+function players() { // set player info and return array of players     **inputs need to be sanitised**
+  let playerOneDefaultOrder = 0;
+  let playerTwoDefaultOrder = 1;
+  
+  const player1 = {
+    name: "Player 1", // temp name
+    mark: "X",   // temp mark
+    identifier: 1,
+    playOrder: playerOneDefaultOrder,
+  }
+  
+  const player2 = {
+    name: "Player 2", // temp name
+    mark: "O",     // temp mark
+    identifier: 2,
+    playOrder: playerTwoDefaultOrder,
+  }
+  return [player1, player2];
+}
 
-// "To start round 'click' who will go first!" [player1] [player2] [random]
-// "CLICK" - start round (player info from fields added to player objects & game board generated)
+//
+// DISPLAY
+//
+function gameDisplay(){
+  function printScreen(){
+    console.log("printScreen");
+  }
+  return { printScreen };
+}
 
-// player 1 place mark
-// player 2 place mark
-//    if row > "congratulations 'winner' you won!"
-//    if board Full && no row > "it's a draw!"
-// reset button (keeps names and marks)
+//
+// GAME FLOW
+//
+function game() {
+  const [player1, player2] = players(); // assign the returned players to usable variables
+  GameBoard.placeMark(player1, 0, 1);
+  GameBoard.placeMark(player2, 1, 1);
+  GameBoard.placeMark(player1, 2, 1);
+  GameBoard.logBoard();
+  gameDisplay.printScreen();
+}
+
+game();
