cf1b0a8ca2e372497978fa5881f4f4089fee1a9d
Steven McIntyre <steven.mc2225@gmail.com>
2024-07-11 16:11:08 -0700

    finished basic game function logic(minus checking for win)
    basic game functionality completed and working MINUS win checking.

--- scripts.js
+++ scripts.js
@@ -16,9 +16,28 @@ const GameBoard = (function(){
     }
   }
 
-  function placeMark(player, row, column) { 
-    // board[row][column] = player.identifier; 
-    board[row][column] = player.mark; // temp
+  // function placeMark(player, row, column) { 
+  //   // board[row][column] = player.identifier;
+  //   if ((board)[row][column] === 0) {
+  //     board[row][column] = player.mark; // temp
+  //   } else {
+  //     placeMark();
+  //   }
+  // }
+  function placeMark(player) { 
+    let [firstCoordinate, secondCoordinate] = prompt(`${player.name}, Where would you like to place your mark?`).split("");
+    if ((board)[firstCoordinate][secondCoordinate] === 0) {
+    // board[row][column] = player.identifier;
+      board[firstCoordinate][secondCoordinate] = player.mark; // temp
+    } else {
+      placeMark(player);
+    }
+    // if (roundNum < 9) {
+    //   console.log(`${player.name}, it's your turn!`);
+    // } else {
+    //   console.log("Game Over!");
+    // }
+
   }
 
   function logBoard() { // log the game board readably
@@ -66,36 +85,73 @@ function players() { // collect player info and return array of players     **in
 // ** GAME FLOW ** 
 //
 function game() {
+  // load players
   const [player1, player2] = players(); // GET "player1" and "player2"
+  // store current player
   let currentPlayer = "";
+  // who's turn to start
   function getFirstPlayer() {
     currentPlayer = prompt("Who wants to go first? Enter 1 for player1, Enter 2 for player2, Leave blank for random.");
     if (currentPlayer !== "1" && currentPlayer !== "2") { // any entry other than 1 or 2 generates a random current player
       currentPlayer = (Math.ceil(Math.random() * 2)).toString();
     };
-    
     switch (currentPlayer) {
       case ("1"):
         currentPlayer = player1;
-        console.log(`${currentPlayer.name}, It's your turn.`);
+        // console.log(`${currentPlayer.name}, It's your turn.`);
         break;
       case ("2"):
         currentPlayer = player2;
-        console.log(`${currentPlayer.name}, It's your turn.`);
+        // console.log(`${currentPlayer.name}, It's your turn.`);
         break;
       default:
         console.log("something went wrong");
     }
     return;
   }
+  // game round logic
+  function playRound() {
+    for (let i = 0; i < 10; i++) {
+      // let [firstCoordinate, secondCoordinate] = prompt("Where would you like to place your mark?").split("");
+      // console.log(firstCoordinate);
+      // console.log(secondCoordinate);
+      // GameBoard.placeMark(currentPlayer, firstCoordinate, secondCoordinate);
+      if (i < 9) {
+        console.log(`${currentPlayer.name}, it's your turn!`);
+        GameBoard.logBoard();
+      } else {
+        console.log("Game Over!");
+        GameBoard.logBoard();
+        break;
+      }
+      GameBoard.placeMark(currentPlayer);
+      switch(currentPlayer.identifier) { // change player when turn is done
+        case 1:
+          currentPlayer = player2;
+          // GameBoard.logBoard();
+          break;
+        case 2:
+          currentPlayer = player1;
+          // GameBoard.logBoard();
+          break;
+        default:
+          console.log("Something borked");
+      }
+      
+    }
+  }
   //
   // game flow >>
   //
-  GameBoard.logBoard(); // print game board
-  // getFirstPlayer();
-  
-  
+  // GameBoard.logBoard(); // print game board
+  getFirstPlayer();
+  playRound();
+  // const [first, second] = "01".split("");
+  // console.log(first);
+  // console.log(typeof(second));
 }
+
+
 
 // RUN GAME
 game();
