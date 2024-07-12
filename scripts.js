e3d6ce02a952304b989b90fa05cdaacb03500be9
Steven McIntyre <steven.mc2225@gmail.com>
2024-07-11 23:40:51 -0700

    using objects instead of simple marks to mark cells
    Each cell is now an object with a mark = 0 and a number ascending from 0>8.
    When a player mark is added to the object, the number value of the cell is
    added to the player's 'played cells' array.
    
    TODO > need to make code to check the played cells array against the possible
    winning combinations that are stored in another array.  I'll use 'split' and
    '.contains' to compare against each group of numbers

--- scripts.js
+++ scripts.js
@@ -11,26 +11,76 @@ const GameBoard = (function(){
   for (let i = 0; i < rows; i++) { 
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i][j] = 0;
+      // board[i][j] = 0; // use this value for production
+      board[i][j] = i; // only use for testing
+      board[i][j] = j; // only use for testing
+      board[i][j] = i*3+j; // only use for testing
+      board[i][j] = {
+        cellName: i*3+j, // only use for testing
+        marker: 0,
+      }
     }
   }
   // place mark on board (and check if location already occupied)
   function placeMark(player) { 
     let [firstCoordinate, secondCoordinate] = prompt(`${player.name}, Where would you like to place your mark?`).split("");
-    if ((board)[firstCoordinate][secondCoordinate] === 0) {
+    if (board[firstCoordinate][secondCoordinate].marker === 0) {
     // board[row][column] = player.identifier;
-      board[firstCoordinate][secondCoordinate] = player.mark; // temp
+      board[firstCoordinate][secondCoordinate].marker = player.mark; // temp
+      player.playedCells.push((board[firstCoordinate][secondCoordinate]).cellName)
+      console.log(player.playedCells);
     } else {
       placeMark(player);
     }
   }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // check for winning combinations and return false OR winning player?
+  function winCheck(player){
+    const winCombinations = [ 012,345,678,036,147,258,048,246 ]
+    console.log("Win Check");
+    winCombinations.forEach(element => {
+      // playedCells.contains > element (use regex) >> win
+      
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   // show gameboard's current state in console
   function logBoard() { // log the game board readably
-    console.log(board[0]);
-    console.log(board[1]);
-    console.log(board[2]);
+    // console.log(board[0]);
+    // console.log(board[1]);
+    // console.log(board[2]);
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i]; // store rows
+      console.log(row);     // print rows
+    }
   }
-  return { placeMark, logBoard};
+  return { placeMark, logBoard, winCheck};
 })();
 
 //
@@ -45,6 +95,7 @@ function players() { // collect player info and return array of players     **in
     mark: "X",   // temp mark
     identifier: 1,
     playOrder: playerOneDefaultOrder,
+    playedCells: [],
   }
   // player two
   const player2 = {
@@ -52,6 +103,7 @@ function players() { // collect player info and return array of players     **in
     mark: "O",     // temp mark
     identifier: 2,
     playOrder: playerTwoDefaultOrder,
+    playedCells: [],
   }
   return [player1, player2];
 }
@@ -116,9 +168,37 @@ function game() {
   //
   // game flow >>
   //
+  
   getFirstPlayer();
   playRound();
+  // GameBoard.winCheck();
 }
 
 // RUN GAME
 game();
+
+
+//
+//
+//
+//
+// NEXT TO DO
+
+// - build 'win checking' logic
+// - build name and mark getting
+// - sanitise inputs
+// - build display function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
